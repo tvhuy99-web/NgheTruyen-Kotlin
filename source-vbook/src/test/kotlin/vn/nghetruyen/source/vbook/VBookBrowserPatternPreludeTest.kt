d4f1b9c5cae160dd80f21e6cd01764a834e57658
tvhuy99-web <tvhuy99@gmail.com>
2026-08-09 15:03:15 +0700

    test: cover browser regex and full html wait

--- source-vbook/src/test/kotlin/vn/nghetruyen/source/vbook/VBookBrowserPatternPreludeTest.kt
+++ source-vbook/src/test/kotlin/vn/nghetruyen/source/vbook/VBookBrowserPatternPreludeTest.kt
@@ -3,7 +3,6 @@ package vn.nghetruyen.source.vbook
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.mozilla.javascript.Context
-import org.mozilla.javascript.ScriptableObject
 
 class VBookBrowserPatternPreludeTest {
     @Test
@@ -14,6 +13,8 @@ class VBookBrowserPatternPreludeTest {
             cx.optimizationLevel = -1
             val scope = cx.initSafeStandardObjects()
             val script = """
+                var Engine = {};
+                var sleep = function(){};
                 var __vbookBrowserMatch = function(){ return false; };
                 ${VBookBrowserPatternPrelude.build()}
                 JSON.stringify([
@@ -24,6 +25,36 @@ class VBookBrowserPatternPreludeTest {
             """.trimIndent()
             val result = Context.toString(cx.evaluateString(scope, script, "browser-pattern-test", 1, null))
             assertEquals("[true,true,false]", result)
+        } finally {
+            Context.exit()
+        }
+    }
+
+    @Test
+    fun browserHtmlHonorsWaitBeyondInternalTwoSecondChunk() {
+        val cx = Context.enter()
+        try {
+            cx.languageVersion = Context.VERSION_ES6
+            cx.optimizationLevel = -1
+            val scope = cx.initSafeStandardObjects()
+            val script = """
+                var slept = [], nativeWait = -1;
+                var sleep = function(ms){ slept.push(Number(ms)); };
+                var Engine = {
+                  newBrowser:function(){
+                    return {html:function(ms){nativeWait=Number(ms);return 'doc';}};
+                  },
+                  browser:function(){
+                    return {html:function(ms){nativeWait=Number(ms);return 'doc';}};
+                  }
+                };
+                var __vbookBrowserMatch = function(){ return false; };
+                ${VBookBrowserPatternPrelude.build()}
+                var result = Engine.newBrowser().html(5000);
+                JSON.stringify({result:result,slept:slept,nativeWait:nativeWait,total:slept.reduce(function(a,b){return a+b;},0)+nativeWait});
+            """.trimIndent()
+            val result = Context.toString(cx.evaluateString(scope, script, "browser-wait-test", 1, null))
+            assertEquals("{\"result\":\"doc\",\"slept\":[2000,1000],\"nativeWait\":2000,\"total\":5000}", result)
         } finally {
             Context.exit()
         }

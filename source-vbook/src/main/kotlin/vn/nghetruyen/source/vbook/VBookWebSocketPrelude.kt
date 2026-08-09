817409b457562f39ea36263352a6a79d6a37816b
tvhuy99-web <tvhuy99@gmail.com>
2026-08-09 14:21:15 +0700

    fix(vbook): apply request-url regex matcher in current host prelude

--- source-vbook/src/main/kotlin/vn/nghetruyen/source/vbook/VBookWebSocketPrelude.kt
+++ source-vbook/src/main/kotlin/vn/nghetruyen/source/vbook/VBookWebSocketPrelude.kt
@@ -1,6 +1,6 @@
 package vn.nghetruyen.source.vbook
 
-/** JavaScript compatibility shim for the current vBook WebSocket ABI. */
+/** JavaScript compatibility shim for the current vBook WebSocket ABI and request-url matching edge. */
 object VBookWebSocketPrelude {
     fun build(): String = """
         var __vbookNativeWebSocket = WebSocket;
@@ -57,5 +57,7 @@ object VBookWebSocketPrelude {
           Object.defineProperty(out, 'closeReason', {get:function(){return nativeSocket.closeReason;}});
           return out;
         };
+
+        ${VBookBrowserPatternPrelude.build()}
     """.trimIndent()
 }

6f784dee44ef6c0cc2eb187aa713479e25db778a
tvhuy99-web <tvhuy99@gmail.com>
2026-08-09 08:11:09 +0700

    build: add source compatibility testkit

---
+++ source-compat-testkit/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins { id("org.jetbrains.kotlin.jvm") }
+
+kotlin { jvmToolchain(17) }
+
+dependencies {
+    implementation(project(":source-api"))
+    testImplementation("junit:junit:4.13.2")
+}

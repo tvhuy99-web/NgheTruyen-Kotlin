684f2879dec4d6a41d874464d8851a0584ee1bd7
tvhuy99-web <tvhuy99@gmail.com>
2026-08-13 23:54:03 +0700

    fix: restore diagnostics after activity destroy callback

---
+++ app/src/main/java/vn/nghetruyen/app/sourceplatform/DiagnosticScreenRestoreLifecycleCallbacks.kt
@@ -0,0 +1,18 @@
+package vn.nghetruyen.app.sourceplatform
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+
+internal class DiagnosticScreenRestoreLifecycleCallbacks : Application.ActivityLifecycleCallbacks {
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) = Unit
+    override fun onActivityStarted(activity: Activity) = Unit
+    override fun onActivityResumed(activity: Activity) = Unit
+    override fun onActivityPaused(activity: Activity) = Unit
+    override fun onActivityStopped(activity: Activity) = Unit
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
+
+    override fun onActivityDestroyed(activity: Activity) {
+        DiagnosticScreenRestoreCoordinator.restoreNext()
+    }
+}

5bf1c958313f8da7d6d5e96f233a0b0b97ed805f
tvhuy99-web <tvhuy99@gmail.com>
2026-08-15 17:59:57 +0700

    test(ui): fix semantics matcher import

--- app/src/androidTest/java/vn/nghetruyen/app/AudioDirectorMusicSettingsUiTest.kt
+++ app/src/androidTest/java/vn/nghetruyen/app/AudioDirectorMusicSettingsUiTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
